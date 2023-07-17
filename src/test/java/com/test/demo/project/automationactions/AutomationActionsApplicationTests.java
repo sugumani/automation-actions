5961b1f5a92bec72e33a791674d9f0a7e0037247
sugumani <35749124+sugumani@users.noreply.github.com>
2023-07-17 08:11:03 -0500

    Add files via upload

---
+++ src/test/java/com/test/demo/project/automationactions/AutomationActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.demo.project.automationactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
